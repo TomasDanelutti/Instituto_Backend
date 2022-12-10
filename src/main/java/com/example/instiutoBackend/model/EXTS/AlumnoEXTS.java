fbd25b1ed1f9029f133f1b79bc16a83c8c5e183c
Tomas Danelutti <tdanelutti@amr.org.ar>
2022-12-10 12:19:50 -0300

    arreglo la clase

--- src/main/java/com/example/instiutoBackend/model/EXTS/AlumnoEXTS.java
+++ src/main/java/com/example/instiutoBackend/model/EXTS/AlumnoEXTS.java
@@ -1,39 +1,10 @@
 package com.example.instiutoBackend.model.EXTS;
 
-import com.example.instiutoBackend.model.Archivo;
-
-import javax.persistence.*;
-import java.util.Date;
-
+import com.example.instiutoBackend.model.Persona;
 public class AlumnoEXTS {
 
-    private Long dni;
+    private Persona persona;
 
     private String clave;
 
-    private String nombre;
-
-    private String apellido;
-
-    private String domicilio;
-
-    private Long telefono;
-
-    private String estadoCivil;
-
-    private String nivelEducativo;
-
-    private String genero;
-
-    private String email;
-
-    private String estado;
-
-    private boolean activo;
-
-    private Date fechaNacimiento;
-
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "idImagen")
-    private Archivo imagen;
 }
