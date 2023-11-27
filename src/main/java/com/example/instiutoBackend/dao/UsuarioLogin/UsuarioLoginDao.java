2a7cc24eb86156df24881aa807d27610bfda307f
Tomas Danelutti <tdanelutti@amr.org.ar>
2023-11-27 20:02:04 -0300

    cambios

--- src/main/java/com/example/instiutoBackend/dao/UsuarioLogin/UsuarioLoginDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.instiutoBackend.dao.UsuarioLogin;
-
-import com.example.instiutoBackend.model.UsuarioLogin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioLoginDao extends JpaRepository<UsuarioLogin, Long> {
-}
